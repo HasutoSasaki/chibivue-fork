8a5bee0df083d60b27b333b9c5e377d0c0d5e88e
HasutoSasaki <hasuto.sa@gmail.com>
2025-05-01 15:11:46 +0900

    feat: Add support for regular props rendering.

--- examples/playground/src/main.ts
+++ examples/playground/src/main.ts
@@ -3,7 +3,7 @@ import { createApp, h } from "chibivue";
 const app = createApp({
     render() {
         return h('div', {}, [
-            h('p', {}, ['Hello world.']),
+            h('p', { style: 'color: blue; font-weight: bold;' }, ['Hello world.']),
             h('button', {
                 onClick() {
                     alert('clicked!')
